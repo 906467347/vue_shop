611bf8810bae984d46a94ad9e326ceb2fd38e6ec
zr906467 <906467347@qq.com>
2020-06-04 22:05:42 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+
 //import Login from '../components/Login.vue'
 // import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 const routes = [
-  { path: '/login', component: () => import('../components/Login.vue')},
-  { path: '/home', component: () => import('../components/home.vue')}
+  { path: '/login', component: () => import('../components/Login.vue') },
+  {
+    path: '/home',
+    component: () => import('../components/home.vue'),
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: () => import('../components/welcome.vue')
+      },
+      {
+        path: '/users',
+        component: () => import('../components/user/Users.vue')
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -15,12 +31,12 @@ const router = new VueRouter({
 })
 
 //挂载路由导航守卫
-router.beforeEach( (to, from, next) => {
+router.beforeEach((to, from, next) => {
   //to 将要访问的路径
   //from 代表从哪个路径跳转而来
   //next 是一个函数，表示放行
   //  next () 放行 next('/login') 强制跳转
-  if(to.path === '/login') return next();
+  if (to.path === '/login') return next();
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
   if (!tokenStr) return next('/login')
